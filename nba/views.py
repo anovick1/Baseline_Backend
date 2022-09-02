1fd7f87e83da638f8f648ab8460e8ae5af510aa2
Avery Novick <averynov@yahoo.com>
2022-09-02 07:34:07 -0400

    views made

--- nba/views.py
+++ nba/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import generics
+from nba.serializers import UserSerializer, PlayerSerializer, StatSerializer, ChartSerializer, CommentSerializer, LikeSerializer
+from nba.models import User, Player, Stat, Chart, Comment, Like
 
 # Create your views here.
+
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class PlayerList(generics.ListCreateAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+
+class PlayerDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
+class StatList(generics.ListCreateAPIView):
+    queryset = Stat.objects.all()
+    serializer_class = StatSerializer
+
+class StatDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Stat.objects.all()
+    serializer_class = StatSerializer
+class ChartList(generics.ListCreateAPIView):
+    queryset = Chart.objects.all()
+    serializer_class = ChartSerializer
+
+class ChartDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Chart.objects.all()
+    serializer_class = ChartSerializer
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+
+class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+class LikeList(generics.ListCreateAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
+
+class LikeDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Like.objects.all()
+    serializer_class = LikeSerializer
